5bc4b9e98972e373b017e1f66562176397afb6fc
geraldomsousaj <geraldo_sousa@icloud.com>
2020-04-07 21:12:56 -0300

    commit

--- Primeira Tarefa.py
+++ Primeira Tarefa.py
@@ -23,7 +23,7 @@ def main():
             print(" You did not type an existing alternative! ")
 
 
-def heat_equation(u0, T, N, _f, lamb):
+def heat_equation(u0, T, N, _f, lamb, g1, g2):
     """
     Fórmula de diferenças finitas:
         N: int (input)
@@ -51,17 +51,19 @@ def heat_equation(u0, T, N, _f, lamb):
     u_old = [u0 for i in range(N+1)] 
     
     u_new = []
+
     
     for k in range(0, M):
         # adicionar u(k+1,0) na u_new
-        u_new.append(0)
+        u_new.append(g1)
         for i in range(1, N):
             u_new.append( u_old[i] + dt * ((u_old[i-1] - 2*u_old[i] + u_old[i+1]) / np.exp2(dx) + _f(k*dt,i*dx)))
         
         # adicionar u(k+1,N) na u_new
-        u_new.append(0)
+        u_new.append(g2)
         u_old = u_new.copy()
         u_new = []        
+
     
     print('-'*15+'Heat Equation done'+'-'*15+'\n')
     return u_old
@@ -75,18 +77,29 @@ def plot(us):
     ax_lamb2 = fig.add_subplot(4, 4, 2)
     ax_lamb3 = fig.add_subplot(4, 4, 3)
     
-    ax_lamb1.scatter([i for i in range(0, len(us[0]))], us[0])
-    ax_lamb2.scatter([i for i in range(0, len(us[0]))], us[1])
-    ax_lamb3.scatter([i for i in range(0, len(us[0]))], us[2])
+    print(us[0])
+    print(us[1])
+    print(us[2]) 
+    
+    def dot_size(n_dots):
+        return [1 for i in range(n_dots)]
+    
+    ax_lamb1.scatter([i for i in range(0, len(us[0]))], us[0], dot_size(len(us[0])))
+    ax_lamb2.scatter([i for i in range(0, len(us[0]))], us[1], dot_size(len(us[0])))
+    ax_lamb3.scatter([i for i in range(0, len(us[0]))], us[2], dot_size(len(us[0])))
 
     #save image as png
-    fig.savefig("figura.png", dpi=1080)
+    fig.savefig("figura.png", dpi=300)
 
 def letra_a():
     T = 1
     lamb_list = [0.25 , 0.5 , 0.51]
     #u(0, x) = u0(x) em [0, 1]
     u0 = 0
+
+    #condicoes de fronteira nulas
+    g1 = 0
+    g2 = 0
 
     try:
         N = int(input("Type N: "))
@@ -96,16 +109,16 @@ def letra_a():
 
 
     def _f(t, x):
-        return 10*(np.exp2(x))*(x - 1) - 60*x*t + 20*t 
+        return 10*(np.power(x, 2))*(x - 1) - 60*x*t + 20*t 
     
     #solucao exata que precisamos nos aproximar:
     def _u(t, x):
-        return 10*t*(np.exp2(x))*(x - 1)
+        return 10*t*(np.power(x, 2))*(x - 1)
 
     us = []
     
     for lamb in lamb_list:
-        u_old = heat_equation(u0, T, N, _f, lamb)
+        u_old = heat_equation(u0, T, N, _f, lamb, g1, g2)
         us.append(u_old)
         
     plot(us)
