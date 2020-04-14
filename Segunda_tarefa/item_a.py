41f566513c5426ba2af557f06504b5800400c847
Tulio <39662335+TmTutui@users.noreply.github.com>
2020-04-14 11:54:14 -0300

    Finish item a Prob 2
    add deprecated annotation

--- Segunda_tarefa/item_a.py
+++ Segunda_tarefa/item_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def decompor_A(principal,sub):
+def decompose_A(principal,sub):
     # Funcao que decompoe a matriz A em L e em D (A = LD(L^-1))
     # Recebe duas listas: a diagonal principal de A e a subdiagonal de A
     # Retorna duas listas: a diagonal Principal de D e a subdiagonal de L
@@ -22,7 +22,9 @@ def decompor_A(principal,sub):
 
     return princ_D,sub_L
 
+@DeprecationWarning
 def dot_product(A,B): # work for two square matrices
+    # Receive two square matrices and return a Matrix
     matrix = np.array([[0 for i in range(len(A))]])
     for i in range(len(A)-1):
         line = np.array([])
@@ -39,7 +41,9 @@ def dot_product(A,B): # work for two square matrices
 
     return matrix
 
+@DeprecationWarning
 def transpose(A): # work for squace matrix
+    # Receive a matrix and return it transposed
     A = np.array(A) #make sure A is a np array not a list
     A_t = A.copy()
 
@@ -49,9 +53,59 @@ def transpose(A): # work for squace matrix
     
     return A_t
 
+def calculate_y(sub_L,b):
+    # Ly = b
+    # Receive sub diagonal of L(list) and the column matrix b and return y
+    y = np.array([b[0]])
+    
+    for i in range(len(b)-1):
+        y = np.append(y, b[i+1] - sub_L[i]*y[i])
+    
+    return y
+
+def calculate_z(diag_D,y):
+    # Dz = y
+    # Receive diagonal of D(list) and the column matrix y and return z
+    z = np.array([])
+    
+    for i in range(len(y)):
+        z = np.append(z, y[i]/diag_D[i])
+    
+    return z
+
+def calculate_x(sub_L,z):
+    # L*(transposed)x = z
+    # Receive super diagonal of L transposed(list) and the column matrix z and return x
+    x = np.zeros(len(z))
+    x[len(x)-1] = z[len(z)-1]
+    
+    for i in range(len(z)-2,-1,-1):
+        x[i] = z[i] - sub_L[i]*x[i+1]
+    
+    return x
+
+def teste4():
+    A = [
+        [32.5,65,0,0],
+        [65,131,5,0],
+        [0,5,28,6],
+        [0,0,6,16],
+    ]
+    b=[
+       5677.75,
+       11477.3,
+       23791.08,
+       61804.16
+    ]
+
+    diag_D, sub_L = decompose_A([32.5,131,28,16],[65,5,6])
+    # Ly = b, Dz = y e L*x = z
+    y = calculate_y(sub_L,b)
+    
+
+    z = calculate_z(diag_D,y)
+    x = calculate_x(sub_L,z)
+
+    print(x)
 
 
-def main():
-
-
-main()
